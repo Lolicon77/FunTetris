fb9aba24e1130317eb73c1243df7480841e238ca
GGWork <1504296509@qq.com>
2018-05-21 18:57:25 +0800

    ~

---
+++ Assets/_Scripts/Game/Element.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game {
+
+	public class Element : MonoBehaviour {
+
+		public ulong Value;
+
+		void Init() {
+
+		}
+
+	}
+}
---
+++ Assets/_Scripts/Game/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Game {
+	public class GameManager : MonoBehaviour {
+
+		public int height;
+
+		public ulong[] allElementDatas;
+
+		void InitVolume() {
+			allElementDatas = new ulong[height];
+		}
+
+		List<int> CheckCrush() {
+			List<int> result = new List<int>();
+			for (int i = 0; i < allElementDatas.Length; i++) {
+				var canCrush = allElementDatas[i] == ulong.MaxValue;
+				if (canCrush) {
+					result.Add(i);
+				}
+			}
+			return result;
+		}
+
+		public void StartGame() {
+			InitVolume();
+		}
+	}
+}
